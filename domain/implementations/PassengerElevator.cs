a31bfe481da3fcbf4745758d7f320e68cb9a1ccb
Troy Clark <TClark@dvtsoftware.com>
2026-06-15 11:47:30 +0200

    feat: implemented elevator exceptions

--- domain/implementations/PassengerElevator.cs
+++ domain/implementations/PassengerElevator.cs
@@ -1,6 +1,7 @@
 namespace domain.implementations;
 
 using domain.enums;
+using domain.exceptions;
 using domain.interfaces;
 
 public class PassengerElevator : IElevator
@@ -23,15 +24,45 @@ public class PassengerElevator : IElevator
 
     public void MoveUp()
     {
-        var nextFloor = _floors.FirstOrDefault(f => f.FloorNumber == CurrentFloor.FloorNumber + 1);
-        if (nextFloor != null)
-            CurrentFloor = nextFloor;
+        var nextFloorNumber = CurrentFloor.FloorNumber + 1;
+        var nextFloor = _floors.FirstOrDefault(f => f.FloorNumber == nextFloorNumber);
+        if (nextFloor == null)
+            throw new FloorOutOfBoundsException(nextFloorNumber);
+        CurrentFloor = nextFloor;
     }
 
     public void MoveDown()
     {
-        var nextFloor = _floors.FirstOrDefault(f => f.FloorNumber == CurrentFloor.FloorNumber - 1);
-        if (nextFloor != null)
-            CurrentFloor = nextFloor;
+        var nextFloorNumber = CurrentFloor.FloorNumber - 1;
+        var nextFloor = _floors.FirstOrDefault(f => f.FloorNumber == nextFloorNumber);
+        if (nextFloor == null)
+            throw new FloorOutOfBoundsException(nextFloorNumber);
+        CurrentFloor = nextFloor;
+    }
+
+    public void Board(IPassenger passenger)
+    {
+        if (BoardedPassengers.Count >= Capacity)
+            throw new ElevatorAtCapacityException(Capacity);
+        BoardedPassengers.Add(passenger);
+    }
+
+    public void Deboard(IPassenger passenger)
+    {
+        BoardedPassengers.Remove(passenger);
+    }
+
+    public void SetDirection(Direction direction)
+    {
+        var topFloor = _floors.Max(f => f.FloorNumber);
+        var bottomFloor = _floors.Min(f => f.FloorNumber);
+
+        if (direction == Direction.Upwards && CurrentFloor.FloorNumber == topFloor)
+            throw new InvalidDirectionException(CurrentFloor.FloorNumber, topFloor);
+
+        if (direction == Direction.Downwards && CurrentFloor.FloorNumber == bottomFloor)
+            throw new InvalidDirectionException(CurrentFloor.FloorNumber, bottomFloor);
+
+        WishDirection = direction;
     }
 }
